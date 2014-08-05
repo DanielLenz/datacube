06300ae11b080fd49fbd43371146a8ab31a9a087
Lars Flöer <lfloeer@LFMBP.local>
2014-08-05 16:27:57 +0200

    Add calculation of moments

--- datacube.py
+++ datacube.py
@@ -1,3 +1,5 @@
+import itertools as it
+
 import numpy as np
 
 from astropy.io import fits
@@ -88,11 +90,9 @@ class Datacube(object):
     
     def _get_velocities(self):
         if (self._velocities is None) or self._calculate_velocities:
-            wcs_sub = self.wcs.sub(['spectral'])
-
             # unsafe, depends on proper alignment of axes
             channels = np.arange(self.data.shape[0])
-            self._velocities = wcs_sub.wcs_pix2world(channels, 0)[0]
+            self._velocities = self.spec_wcs.wcs_pix2world(channels, 0)[0]
             self._calculate_velocities = False
 
         return self._velocities
@@ -101,8 +101,24 @@ class Datacube(object):
     velocities = property(_get_velocities)
 
 
-    def moment(self, vslice=None, cslice=None, kind=[0], mask=None):
-        pass
+    def moment(self, vslice=None, cslice=None, kind=0, mask=None):
 
+        if mask is None:
+            mask = 1.
+        elif mask.shape == self.data.shape[1:]:
+            mask = mask[None]
+        
+        if vslice is not None:
+            cslice = self.spec_wcs.wcs_world2pix(vslice, 0)
 
+        if cslice is not None:
+            cslice = [int(f(c)) for f,c in it.izip([np.floor, np.ceil], cslice)]
+            data_slice = slice(*cslice)
 
+            if kind == 0:
+                return np.sum(self.data[data_slice] * mask, 0)
+
+            if kind == 1:
+                m = np.sum(self.data[data_slice] * self.velocities[data_slice] * mask, 0)
+                m /= np.sum(self.data[data_slice] * mask, 0)
+                return m
