0fc0358ba761dada1db22c22ca48fd692caee296
Lars Flöer <lfloeer@LFMBP.local>
2014-08-09 19:49:01 +0200

    Add support for Jy/pixel conversion

--- utilities.py
+++ utilities.py
@@ -1,11 +1,12 @@
 from astropy import units as u
 from astropy import constants as const
 
-def brightness_temperature_jybeam(beam_area, rest_frequency):
+def brightness_temperature_jybeam(pixel_area, beam_area, rest_frequency):
     """
     Defines the conversion between Jy/beam and "brightness temperature"
     """
     beam = beam_area.to(u.sr).value
+    pixel = pixel_area.to(u.sr).value
     nu = rest_frequency.to(u.GHz, u.spectral())
 
     def convert_Jyb_to_K(x_jybm):
@@ -16,7 +17,18 @@ def brightness_temperature_jybeam(beam_area, rest_frequency):
         factor = (u.Jy / (2 * const.k_B * nu**2 / const.c**2)).to(u.K).value
         return (x_K * beam / factor)
 
-    return [(u.Jy / u.beam, u.K, convert_Jyb_to_K, convert_K_to_Jyb)]
+    def convert_Jyp_to_K(x_jybm):
+        factor = (2 * const.k_B * u.K * nu**2 / const.c**2).to(u.Jy).value
+        return (x_jybm / pixel / factor)
+
+    def convert_K_to_Jyp(x_K):
+        factor = (u.Jy / (2 * const.k_B * nu**2 / const.c**2)).to(u.K).value
+        return (x_K * pixel / factor)
+
+    return [
+        (u.Jy / u.beam, u.K, convert_Jyb_to_K, convert_K_to_Jyb),
+        (u.Jy / u.pixel, u.K, convert_Jyb_to_K, convert_K_to_Jyb)
+    ]
 
 
 def first_match(keys, d):
